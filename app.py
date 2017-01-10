84f0283be7d2f1db275bf03bb2315762d76f7a91
laurenfb <laurenfriesbrink@gmail.com>
2017-01-10 15:31:10 -0800

    added error handling within flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 from ravelry_api_wrapper import *
 
 
@@ -11,7 +11,15 @@ def index():
 @app.route('/api/get_projects/<username>', methods=['GET'])
 def get_projects(username):
     projects = APIWrapper.get_current_user_projects(username)
+    if type(projects) is int:
+        # will return error code if there is one, so we'll use the flask error handler here.
+        abort(projects)
     return jsonify(projects)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True) # this will be false on production
