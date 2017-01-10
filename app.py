83560d681e904d0de20b5d982abfb2f10149060c
laurenfb <laurenfriesbrink@gmail.com>
2017-01-10 14:09:48 -0800

    we have an API yay\!\!\!\!\!

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/api/get_projects/<username>', methods=['GET'])
+def get_projects(username):
+    projects = APIWrapper.get_current_user_projects(username)
+    return jsonify(projects)
+
 if __name__ == '__main__':
     app.run(debug=True) # this will be false on production
--- ravelry_api_wrapper.py
+++ ravelry_api_wrapper.py
@@ -1,6 +1,10 @@
 from config import *
+import requests
+
+URL = 'https://api.ravelry.com/'
 
 class APIWrapper:
     @staticmethod
-    def cat():
-        return RAVELRY_ACCESS_KEY
+    def get_current_user_projects(current_username):
+        r = requests.get(URL + 'projects/' + current_username + '/list.json', auth=(RAVELRY_ACCESS_KEY, RAVELRY_PERSONAL_KEY))
+        return r.json()
