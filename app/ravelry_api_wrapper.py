ac1c37d25da38dfeb22f77c6839575dbf29a33c2
laurenfb <laurenfriesbrink@gmail.com>
2017-01-15 21:31:22 -0800

    YAAAAAAAS we are writing projects to the database\! BUT it is currently writing duplicate projects, fix that

--- app/ravelry_api_wrapper.py
+++ app/ravelry_api_wrapper.py
@@ -1,5 +1,5 @@
 import requests
-from config import RAVELRY_ACCESS_KEY, RAVELRY_PERSONAL_KEY
+from config import *
 from app import db
 from models import *
 
@@ -19,43 +19,81 @@ class APIWrapper:
                 user = User(name = current_username, email = "not sure how we're getting this", photo_url = user_info.json()["user"]["large_photo_url"], imported = True)
                 db.session.add(user)
                 db.session.commit()
-            # return {"clusters": sort_projects(r.json()["projects"], user.id)} #user.id is available after the user has been saved.
+            # return {"clusters": APIWrapper.sort_projects(r.json()["projects"], user.id)} #user.id is available after the user has been saved.
+            # print r.json()["projects"]
             return r.json()["projects"]
         else:
             return r.status_code
 
-
+    @staticmethod
     def sort_projects(projects, userID):
         ### @TODO this is 100% non fucntional rn, but need to get the projects as they show up and save them in the database. also save user, and clusters.
         to_return = []
         for project in projects:
-            # project must be finished.
-            if project["status_name"] == "Finished":
-                # it will either have a pattern, or not have a pattern.
-                if project["pattern_name"] == "null": # or None? or null with no quotes?
-                    # if there's no cluster called misc for this user...
-                    misc_cluster = Cluster.query.filter_by(user_id = userID, name="misc").first()
-                    if misc_cluster is None:
-                        # make the cluster.
-                        misc_cluster = Cluster(name = "misc", user_id = userID, avg_days = None)
-                        db.session.add(misc_cluster)
-                        db.session.commit()
-                        # now the misc cluster is saved, so it has an ID. use that to make a new project in the database, using the cluster's ID
-                        project = Project(name = project["name"], photo_url = project["square_url"], time_in_days = calc_time_in_days(project), user_id = userID, cluster_id = misc_cluster.id, pattern_id = None)
-                        db.session.add(project)
+            # project must be finished. 2 is Rav's project_status_id for finshed. 1 is WIP, 3 is hibernating, 4 is frogged, in case that matters later.
+            if project["project_status_id"] == 2:
+                # it will either have a pattern, or not have a pattern. deal with the projects that don't belong to a pattern first.
+                if project["pattern_name"] is None: # json conversion  in requests makes null into None
+                    APIWrapper.sort_proj_no_pattern(project, userID)
                 else:
-                    # get the project's pattern id,
-                    # check to see if there is a cluster with that name under the user's name.
-                    # if there is not, make the cluster and give the the appropriate name
-                    # if there is, make the project in the database and add it to the appropriate cluster
+                    APIWrapper.sort_proj_with_pattern(project, userID)
+        # then return a list of clusters with projects as lists inside of those.
+        clusters = Cluster.query.filter_by(user_id = userID).all()
+        for cluster in clusters:
+            to_return.append({cluster.name: Project.query.filter_by(cluster_id = cluster.id).all()})
+        return to_return
 
-                    project = Project(name = project["name"], photo_url = project["square_url"], time_in_days = calc_time_in_days(project), user_id = userID, cluster_id = cluster.id, pattern_id = None)
-                    db.session.add(project)
+    @staticmethod
+    def sort_proj_no_pattern(project, userID):
+        # look for a cluster with title misc belonging to this user
+        misc_cluster = Cluster.query.filter_by(user_id = userID, name="misc").first()
+        if misc_cluster is None:
+            # if they don't yet have one, make the cluster.
+            misc_cluster = Cluster(name = "misc", user_id = userID, avg_days = None)
+            db.session.add(misc_cluster)
+            db.session.commit()
+            # now the misc cluster is saved, so it has an ID. use that to make a new project in the database, using the cluster's ID
+            project = Project(name = project["name"], photo_url = project["first_photo"]["square_url"], time_in_days = calc_time_in_days(project), user_id = userID, cluster_id = misc_cluster.id, pattern_id = None)
+            db.session.add(project)
         db.session.commit()
 
+    @staticmethod
+    def sort_proj_with_pattern(project, userID):
+        #  get the project's pattern id,
+        pattern_id = project["pattern_id"]
+        # check to see if that pattern is in the database. if it's not, call the Rav API and put it in the database.
+        pattern = Pattern.query.filter_by(id = pattern_id).first()
+        if pattern is None:
+            pattern = APIWrapper.single_pattern_call(pattern_id)
+            db.session.add(pattern)
+        # pattern is already in the database
+        else:
+            # check to see if there is a cluster with that name under the user's name. cluster names are based on pattern categories.
+            cluster = Cluster.query.filter_by(user_id = userID, name = pattern.category).first()
+            # if there is not, make the cluster and give the the appropriate name
+            if cluster is None:
+                cluster = Cluster(name = pattern.category, user_id = userID, avg_days = None)
+                # add it to the session, save that session so that I can use that cluster's ID to make the project.
+                db.session.add(cluster)
+                db.session.commit()
+            # now the cluster does exist, even if I just had to make it, so use that cluster ID to make the project.
+            new_project = Project(name = project["name"], photo_url = project["first_photo"]["square_url"], time_in_days = APIWrapper.calc_time_in_days(project), user_id = userID, cluster_id = cluster.id, pattern_id = pattern.id)
+            print new_project
+            db.session.add(new_project)
+        db.session.commit()
+
+    @staticmethod
+    def single_pattern_call(pattern_id):
+        r = requests.get(URL + "patterns/" + str(pattern_id) + ".json", auth=(RAVELRY_ACCESS_KEY, RAVELRY_PERSONAL_KEY))
+        if len(r.json()["pattern"]["pattern_categories"]) > 0:
+            category = APIWrapper.get_pattern_type(r.json()["pattern"]["pattern_categories"][0])
+        else:
+            category = "misc"
+        pattern = Pattern(id = pattern_id, name = r.json()["pattern"]["name"], category = category)
+        return pattern
 
 
-
+    @staticmethod
     def get_pattern_type(pattern_category):
         name = pattern_category["name"].lower()
         if name == "other":
@@ -66,7 +104,7 @@ class APIWrapper:
             category = "gloves / mittens"
         elif name in NECK:
             category = "misc neckwear"
-        elif name in HEADWEAR:
+        elif name in HEAD:
             category = "misc headwear"
         elif name in HOME:
             category = "home items"
@@ -85,6 +123,7 @@ class APIWrapper:
             category = name
         return category
 
+    @staticmethod
     def deal_with_other(pattern_category):
         parent = pattern_category['parent']['name'].lower()
         if parent not in TOP_LEVEL:
@@ -104,16 +143,11 @@ class APIWrapper:
             category = pattern_category['name'].lower()
         return category
 
+    @staticmethod
     def calc_time_in_days(project):
         return 10
-# #
-# user = User.query.filter_by(name='laureneliz').first()
-# print user is None
-# clusters = Cluster.query.filter_by(user_id = user.id, name="misc").first()
-# print clusters
-# print len(clusters)
-# pattern = Pattern.query.filter_by(name='Hitchhiker').first()
-# print pattern
-# print pattern.name
-#
+
 # APIWrapper.import_user("laureneliz")
+# APIWrapper.sort_projects(laurenprojects["projects"], 1)
+# for project in laurenprojects["projects"]:
+#     print project["project_status_id"]
