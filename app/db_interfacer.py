efb7c155a4239c882696542648d461e56fca318d
laurenfb <laurenfriesbrink@gmail.com>
2017-01-22 22:27:42 -0800

    PUT route now updates projects and nicely returns them\!

--- app/db_interfacer.py
+++ app/db_interfacer.py
@@ -1,11 +1,22 @@
 from config import *
 from app import db
-from models import *
+from models import Project, Cluster
+from flask import request
 
 class DBInterfacer:
     @staticmethod
     def take_in_projects(request):
-        return {'hi': 'this is totally a json'}
+        clusters = request.get_json()['clusters']
+        changed_projects = []
+        for cluster in clusters:
+            for p in cluster['projects']:
+                project = Project.query.filter_by(id = p['id']).first()
+                if project is not None:
+                    project.cluster_id = cluster['id']
+                    changed_projects.append({"name": project.name, "id": project.id, "cluster_id": project.cluster_id})
+                    db.session.add(project)
+        db.session.commit()
+        return {'success': changed_projects}
 
     @staticmethod
     def archive_project(project):
