15e1fbab8c00d013bb5ae75ee2c12d122e7fdb4e
laurenfb <laurenfriesbrink@gmail.com>
2017-01-23 23:26:26 -0800

    new function for new route, and updated funcion to change projects

--- app/db_interfacer.py
+++ app/db_interfacer.py
@@ -6,17 +6,17 @@ from flask import request
 class DBInterfacer:
     @staticmethod
     def take_in_projects(request):
-        clusters = request.get_json()['clusters']
+        projects = request.get_json()['projects']
         changed_projects = []
-        for cluster in clusters:
-            for p in cluster['projects']:
-                project = Project.query.filter_by(id = p['id']).first()
-                if project is not None:
-                    project.cluster_id = cluster['id']
-                    changed_projects.append({"name": project.name, "id": project.id, "cluster_id": project.cluster_id})
-                    db.session.add(project)
+        for project in projects:
+            p = Project.query.filter_by(id = project['id']).first()
+            if p is not None:
+                p.cluster_id = project['clusterID']
+                changed_projects.append(p)
+                db.session.add(p)
         db.session.commit()
-        return {'success': changed_projects}
+        # print changed_projects
+        return {'success': [project.as_dict() for project in changed_projects]}
 
     @staticmethod
     def archive_project(project):
@@ -30,3 +30,10 @@ class DBInterfacer:
             db.session.commit()
             response = {'success': project_object.name + ' archived'}
         return response
+
+    @staticmethod
+    def mark_imported(user):
+        user.imported = True
+        db.session.add(user)
+        db.session.commit()
+        return {'success': user.name + ' imported'}
